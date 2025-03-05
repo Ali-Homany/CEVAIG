18ae549358b2ebaaf3955b02c91eb31d1d492daf
Ali Houmani <houmani.datalu@gmail.com>
2025-03-05 04:45:13 +0200

    Improve Explainer
    Explainer work is now divided into 2 steps, first agents explains the codebase and writes it as json (filepath + explanation), then another agent reads the json and appends start_line & end_line attributes to highlight lines under focus. So new prompt & method were added to handle the second agent, while codebase parsing is slightly updating to make highlighting lines easier.

--- src/explainer/__init__.py
+++ src/explainer/__init__.py
@@ -23,9 +23,34 @@ class Explanation(BaseModel):
 def explain_codebase(dir_path: str) -> str:
     codebase_str = get_codebase(dir_path)
     llm = Gemini(os.getenv('GENAI_API_KEY'))
-    with open('./src/explainer/prompt.txt', 'r') as f:
+    with open('./src/explainer/prompt_explain.txt', 'r') as f:
         prompt = f.read()
     prompt = prompt.format(codebase=codebase_str)
     explanations = llm.generate(prompt, response_schema=list[Explanation])
     # explanations = [explanation.dict() for explanation in explanations]
     return explanations
+
+
+def add_highlighting(dir_path: str, explanations: list[dict], max_highlight: int=30) -> list[dict]:
+    codebase_str = get_codebase(dir_path, show_line_numbers=True)
+    llm = Gemini(os.getenv('GENAI_API_KEY'))
+    with open('./src/explainer/prompt_highlight.txt', 'r') as f:
+        prompt = f.read()
+    prompt = prompt.format(codebase=codebase_str, explanations=explanations, max_highlight=max_highlight)
+    updated_explanations = llm.generate(prompt, response_schema=list[Explanation])
+    return updated_explanations
+
+
+if __name__ == '__main__':
+    import json
+    explanations = explain_codebase(
+        './testing/sample_code/'
+    )
+    with open('./temp/explanations.json', 'w') as f:
+        json.dump(explanations, f, indent=4)
+    explanations = add_highlighting(
+        './testing/sample_code/',
+        explanations
+    )
+    with open('./temp/explanations_2.json', 'w') as f:
+        json.dump(explanations, f, indent=4)
--- src/explainer/codebase_parser.py
+++ src/explainer/codebase_parser.py
@@ -6,12 +6,15 @@ This module is responsible for wrapping up the codebase for a model
 """
 
 
-def read_code_file(file_path: str) -> str:
+def read_code_file(file_path: str, add_line_numbers: bool=False) -> str:
     with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
+        code = file.read()
+    if add_line_numbers:
+        code = '\n'.join([f'{i+1}: {line}' for i, line in enumerate(code.split('\n'))])
+    return code
 
 
-def get_codebase(dir_path: str, allowed_files: list=('py', 'html', 'css', 'md')) -> str:
+def get_codebase(dir_path: str, show_line_numbers: bool=False, allowed_files: list=('py', 'html', 'css', 'md')) -> str:
     codebase = ''
     for root, _, files in os.walk(dir_path):
         for file in files:
@@ -19,9 +22,9 @@ def get_codebase(dir_path: str, allowed_files: list=('py', 'html', 'css', 'md'))
             if file_extension in allowed_files:
                 file_path = os.path.join(root, file)
                 relative_file_path = os.path.relpath(file_path, dir_path)
-                codebase += f'\n\n{relative_file_path}:\n```\n{read_code_file(file_path)}```'
+                codebase += f'\n\n{relative_file_path}:\n```\n{read_code_file(file_path, add_line_numbers=show_line_numbers)}\n```\n'
     return codebase
 
 
 if __name__ == '__main__':
-    print(get_codebase('./testing/sample_code/'))
+    print(get_codebase('./testing/sample_code/', show_line_numbers=True))
